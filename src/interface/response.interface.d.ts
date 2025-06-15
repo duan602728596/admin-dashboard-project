45ff80cb7494f1d47f8875dbad87b37e2edf7e80
duan602728596 <duanhaochen@126.com>
2025-06-15 20:11:02 +0800

    refactor: 删除旧的接口定义类型

--- src/interface/response.interface.d.ts
+++ src/interface/response.interface.d.ts
@@ -1,18 +1,3 @@
-/* 定义一个响应成功的接口 */
-export interface SuccessResponseData<T> {
-  code: 1; // 1：响应成功
-  data: T; // 返回的数据
-}
-
-/* 定义一个响应失败的接口 */
-export interface ErrorResponseData {
-  code: 0; // 0：响应失败
-  errorMessage: string;
-}
-
-/* 定义一个通用的响应的接口 */
-export type CoreResponseData<T> = SuccessResponseData<T> | ErrorResponseData;
-
 export interface GraphQLSuccessResponseData<T> {
   data: T; // 返回的数据
 }
